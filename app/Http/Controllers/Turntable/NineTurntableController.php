fd5904147090740608ebe6e911ff7cbdafda0e9b
xuyz <756288646@qq.com>
2018-07-06 17:02:25 +0800

    ts

--- app/Http/Controllers/Turntable/NineTurntableController.php
+++ app/Http/Controllers/Turntable/NineTurntableController.php
@@ -110,7 +110,7 @@ class NineTurntableController extends Controller
             })->toArray();
             //中奖率数组
             $prizeRates=$turntable->prizes->map(function ($prize) {
-                return $prize->PrizeRate*0.01;
+                return round($prize->PrizeRate*0.01,2);
             })->toArray();
             Log::info(json_encode($prizeRates));
             $alias=new AliasMethod($prizeRates);
