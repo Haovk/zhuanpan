4891c7704b4caa69e3ed5dfd91eb789d9576e455
xuyz <756288646@qq.com>
2018-07-06 17:55:19 +0800

    ts

--- app/Http/Controllers/Turntable/NineTurntableController.php
+++ app/Http/Controllers/Turntable/NineTurntableController.php
@@ -172,7 +172,7 @@ class NineTurntableController extends Controller
         $user = session('wechat.oauth_user.default');
         $turntable=Turntable::find($request->id);//获取转盘信息
         $tuser=$turntable->turntableUsers->where('OpenId', $user->id)->first();//获取当前用户信息
-        $prizeLogs=$tuser->prizeLogs->all();
+        $prizeLogs=$tuser->prizeLogs->where('IsGive')->sortByDesc('CreateTime')->all();
         return json_encode($prizeLogs);
     }
     public function getAllTickets(Request $request)
